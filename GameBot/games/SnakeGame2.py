1ccd54e6ccf56b6cdf4cc7b8ce77d232b3026835
Timur Bikbulatov <ch7cly@gmail.com>
2022-11-12 00:40:33 +0300

    sd

--- GameBot/games/SnakeGame2.py
+++ GameBot/games/SnakeGame2.py
@@ -1,3 +1,5 @@
+
+#%%
 import time
 import numpy as np
 from gym import Env
@@ -11,6 +13,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
 import keyboard
+import matplotlib.pyplot as plt
 #%% md
 #%%
 def actionAsk():
@@ -67,6 +70,7 @@ class CustomEnv(Env):
         self.food = Point2(0, 0)
         self.aim = Point2(0, -1)
         self.endGame = False
+        self.memory = 0
 
     def inside(self, head):
         return -1 < head.x < self.WIDTH and -1 < head.y < self.HEIGHT
@@ -90,10 +94,9 @@ class CustomEnv(Env):
     def step(self, action):
         self.change(action)
         head = self.snake[-1] + self.aim
-        print('\rAction is '+str(action))
         if (not self.inside(head) or head in self.snake) and self.aim != Point2(0, 0):
             self.endGame = True
-            reward = -10
+            reward = 0.5
             done = True
         else:
             done = False
@@ -102,19 +105,22 @@ class CustomEnv(Env):
             if head == self.food:
                 while self.food in self.snake:
                     self.food = self.rPoint()
-                reward = 70
+                reward = 0.000001
             else:
                 prev = np.array([np.abs(self.snake[-2].x - self.food.x), np.abs(self.snake[-2].y - self.food.y)])
                 cur = np.array([np.abs(self.snake[-1].x - self.food.x), np.abs(self.snake[-1].y - self.food.y)])
 
                 if prev[0] < cur[0] or prev[1] < cur[1]:
-                    reward = -5
+                    reward = 0.29
+                elif np.abs(self.memory-action) == 2:
+                    reward = 0.2
                 else:
-                    reward = 5
+                    reward = 0.09
                 self.snake.pop(0)
 
         # Setting the placeholder for info
         info = len(self.snake)
+        self.memory = action
         # Returning the step information
         return self.snakePos(), reward, done, info
 
@@ -169,11 +175,15 @@ class CustomEnv(Env):
 #%%
 h = w = 10
 env = CustomEnv(h, w)
-
+#rule
+#likirelu
+#sigmoid
+#th
 inputs = keras.Input(shape=(h * w,), name="digits")
-x1 = layers.Dense(80, activation="relu")(inputs)
-x2 = layers.Dense(80, activation="relu")(x1)
-outputs = layers.Dense(4, name="predictions")(x2)
+x1 = layers.Dense(128, activation="relu")(inputs)
+x2 = layers.Dense(64, activation="relu")(x1)
+x3 = layers.Dense(64, activation="relu")(x2)
+outputs = layers.Dense(4, name="predictions")(x3)
 model = keras.Model(inputs=inputs, outputs=outputs)
 #%%
 # Instantiate an optimizer.
@@ -182,19 +192,27 @@ optimizer = keras.optimizers.SGD(learning_rate=1e-3)
 loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 #%%
-epochs = 50
+epochs = 500
+allReward = []
+allScores = []
+allSteps = []
+allDicts = []
 for epoch in range(epochs):
     print("\nStart of epoch %d" % (epoch,))
     done = False
     reward = 0
     agent = env.reset()
     score = 0
-    allreward = 0
+    sumReward = 0
     step = 0
     info = 1
+    dict = {1:0,
+        2:0,
+        3:0,
+        0:0}
     # Iterate over the batches of the dataset.
-    while not done:
-
+    #for i in range(100):
+    while not done and step < 100:
         # Open a GradientTape to record the operations run
         # during the forward pass, which enables auto-differentiation.
         with tf.GradientTape() as tape:
@@ -210,8 +228,8 @@ for epoch in range(epochs):
             # on the GradientTape.
             logits = model(input, training=True)  # Logits for this minibatch
             agent, reward, done, score = env.step(np.argmax(logits[0]))
-            allreward = allreward + reward
-            env.render(epochs,allreward)
+            sumReward = sumReward + reward
+            #env.render(epoch,allreward)
             step = step + 1
             # Compute the loss value for this minibatch.
             #loss_value = loss_fn(y_batch_train, logits)
@@ -225,12 +243,88 @@ for epoch in range(epochs):
         # Run one step of gradient descent by updating
         # the value of the variables to minimize the loss.
         optimizer.apply_gradients(zip(grads, model.trainable_weights))
-
+        dict[np.argmax(logits[0])] = dict[np.argmax(logits[0])] +1
         # Log every 200 batches.
-        if step % 30 == 0:
-            print(
-                "Training loss (for one batch) at step %d: %.4f"
-                % (step, float(reward))
-            )
+        print(
+            '\r'+"Epi %d Training loss (for one batch) at step %d: %.4f"
+            % (epoch,step, float(sumReward))
+
+        )
+        print('\rdown  is'+str(dict[0]))
+        print('\rleft  is'+str(dict[1]))
+        print('\rup    is'+str(dict[2]))
+        print('\rright is'+str(dict[3]))
+        clear_output(wait=True)
+
+    allReward.append(sumReward)
+    allScores.append(len(env.snake))
+    allSteps.append(step)
+    allDicts.append(dict)
+#%%
+scorestep = []
+rewardstep = []
+down = []
+left = []
+up =[]
+right = []
+for i in range(epochs):
+    scorestep.append(float(allScores[i])/float(allSteps[i]))
+    rewardstep.append(float(allReward[i])/float(allSteps[i]))
+    down.append( allDicts[i][0] / (allDicts[i][0] + allDicts[i][1] + allDicts[i][2] + allDicts[i][3]))
+    left.append( allDicts[i][1] / (allDicts[i][0] + allDicts[i][1] + allDicts[i][2] + allDicts[i][3]))
+    up.append(   allDicts[i][2] / (allDicts[i][0] + allDicts[i][1] + allDicts[i][2] + allDicts[i][3]))
+    right.append(allDicts[i][3] / (allDicts[i][0] + allDicts[i][1] + allDicts[i][2] + allDicts[i][3]))
+plt.title('score/step')
+plt.plot(scorestep)
+#%%
+plt.title('reward/step')
+plt.plot(rewardstep)
+#%%
+plt.plot(allReward,color='red')
+plt.plot(allSteps,color='blue')
+plt.title('reward and steps')
+#%%
+downl, = plt.plot(down,label = 'down')
+upl, = plt.plot(up, label = 'up')
+plt.legend(handles=[downl,upl])
+
+#%%
+leftl, = plt.plot(left,label ='left')
+rightl, = plt.plot(right, label = 'right')
+plt.legend(handles=[leftl,rightl])
+#%%
+downl, = plt.plot(down,label = 'down')
+leftl, = plt.plot(left,label ='left')
+upl, = plt.plot(up, label = 'up')
+rightl, = plt.plot(right, label = 'right')
+plt.legend(handles=[downl,leftl,upl,rightl])
+#%%
+episodes = 500
+rrr = []
+for epi in range(episodes):
+    print("\nStart of epoch %d" % (episodes,))
+    done = False
+    reward = 0
+    agent = env.reset()
+    score = 0
+    allreward = 0
+    step = 0
+    info = 1
+    env.render(epi,score)
+    # Iterate over the batches of the dataset.
+    while not done and step < 100:
+        input = tf.convert_to_tensor(np.array([agent]), dtype=tf.float32)  # Logits for this minibatch
+        action = model.predict(input)
+        agent, reward, done, score = env.step(np.argmax(action))
+        env.render(epi,score)
+        allreward = allreward + reward
+        step = step + 1
+        sleep(0.05)
+        # Log every 200 batches.
+        print(
+            '\r'+"Epi %d Training loss (for one batch) at step %d: %.4f"
+            % (epi,step, float(allreward))
+        )
+    rrr.append(allreward)
     print("Seen so far: %s samples" % epoch)
-    print("Score is: %s" % score)
+    print("Score is: %s" % info)
