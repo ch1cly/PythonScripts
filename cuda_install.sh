87710353b03569b94f71bfd0ef3ef1c3fbea44b0
Timur Bikbulatov <57445583+ch1cly@users.noreply.github.com>
2024-08-06 14:58:40 +0300

    Update cuda_install.sh

--- cuda_install.sh
+++ cuda_install.sh
@@ -1,9 +1,12 @@
 sudo apt-get remove --purge '^nvidia-.*'
+sudo apt autoremove
 dpkg -l | grep -i nvidia
 
 sudo add-apt-repository ppa:graphics-drivers/ppa
 
 sudo apt update
+apt search nvidia-driver
+
 
 sudo apt install nvidia-driver-550-server
 sudo apt install nvidia-cuda-toolkit
